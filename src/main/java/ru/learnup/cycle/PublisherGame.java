9609b4be0bc30335faef1eeab8983f249a49dfb4
Rowertom <96700458+Rowertom@users.noreply.github.com>
2022-03-30 16:58:56 +0300

    Initial commit

---
+++ src/main/java/ru/learnup/cycle/PublisherGame.java
@@ -0,0 +1,23 @@
+package ru.learnup.cycle;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+
+public class PublisherGame implements ApplicationContextAware {
+
+
+    private ApplicationContext context;
+
+
+    public void publisherGame(int number) {
+        context.publishEvent(new GameEvent(number));
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.context = applicationContext;
+    }
+}
---
+++ src/main/java/ru/learnup/cycle/Test.java
@@ -0,0 +1,31 @@
+package ru.learnup.cycle;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Random;
+
+@Slf4j
+public class Test implements ApplicationContextAware {
+
+    private ApplicationContext context;
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("configuration.xml");
+
+
+        PublisherGame publisher = context.getBean(PublisherGame.class);
+        publisher.publisherGame(new Random().nextInt(1000));
+
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.context = applicationContext;
+    }
+}
+
